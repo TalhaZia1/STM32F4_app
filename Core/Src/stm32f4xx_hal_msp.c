3f6c6e66c2dedc11daf8d9a36738751351639b60
TalhaZia1 <TalhaZia1@users.noreply.github.com>
2022-12-31 14:41:36 +0100

    Low Level Processor Specific Hardware Init

--- Core/Src/stm32f4xx_hal_msp.c
+++ Core/Src/stm32f4xx_hal_msp.c
@@ -1,1 +1,28 @@
+#include "stm32f4xx_hal.h"
 
+/**
+ * TODO: Low level Processor Specific Inits 
+ * -> Priority Grouping of ARM Cortex MX Processor
+ * -> Enabling required system exceptions 
+ * -> Configuring priority for system exceptions 
+*/
+
+void HAL_MspInit(void) {
+    /**
+     * Priority Grouping -> Defualt for PriorityGroup-4
+    */
+    HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+    
+    /**
+     * Enabling Usage Fault, Memoery Fault and Bus Fault System Exceptions
+    */
+    SCB->SHCSR |= 0x7 << 16; 
+
+    /**
+     * Setting Priority of Systems Exceptions
+    */
+    HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+
+}
