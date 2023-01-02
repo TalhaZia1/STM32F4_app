e6d34d88fc19d37fa39f65922985d416659e6f37
TalhaZia1 <TalhaZia1@users.noreply.github.com>
2023-01-02 23:08:44 +0100

    PLL Cfg with HSI

--- Core/Inc/clock_cfg.h
+++ Core/Inc/clock_cfg.h
@@ -4,5 +4,6 @@
 #include "stm32f4xx_hal.h"
 
 void HSE_Clock_Config(void);
+void PLL_Clock_Config(void);
 
 #endif /* __CLOCK_CFG_H */
--- Core/Src/clock_cfg.c
+++ Core/Src/clock_cfg.c
@@ -39,3 +39,48 @@ void HSE_Clock_Config(void) {
 	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
 }
+
+/**
+ * TODO: PLL Configuration Settings
+ * Configuration Settings are hard coded
+ * Function call in main.c @ref PLL_Clock_Config()
+*/
+
+void PLL_Clock_Config(void) {
+
+    RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+	
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	osc_init.HSIState = RCC_HSI_ON;  
+	osc_init.HSICalibrationValue = 16; 
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	osc_init.PLL.PLLM = 16;
+	osc_init.PLL.PLLN = 100;
+	osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+	osc_init.PLL.PLLQ = 2;
+	osc_init.PLL.PLLR = 2;
+	if (HAL_RCC_OscConfig(&osc_init) != HAL_OK) {
+		while(1);
+	}
+
+	/* Divider Settings */
+	clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	clk_init.APB1CLKDivider = RCC_SYSCLK_DIV2;
+	clk_init.APB2CLKDivider = RCC_SYSCLK_DIV2;
+
+	/* Switching from HSI to PLL */
+	if (HAL_RCC_ClockConfig(&clk_init, FLASH_ACR_LATENCY_1WS) != HAL_OK) {
+		while(1);
+	} 
+	
+	/**
+	 * SysTick -> Every 1msec. 
+	 * Reconfiguration as of new HCLK
+	*/
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+}
