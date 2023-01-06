62bda19dfe8fe03d89cd765400af0038dcd18125
TalhaZia1 <TalhaZia1@users.noreply.github.com>
2023-01-06 15:11:58 +0100

    code cleanup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,19 +1,13 @@
 #include "stm32f4xx_hal.h"
 #include "clock_cfg.h"
-#include "timer_cfg.h"
 #include "gpio_cfg.h"
 #include "uart_cfg.h"
 
 int main(void) {
 	HAL_Init();
-	PLL_Clock_Config();
 	UART2_Init();
 	printWelcomeMessage();
-	TIMER2_Init_PWM();
-	TIMER2_Start_PWM();
-	while (1) {
-		brightnessControl();
-	}
+	while (1);
 	return 0; 
 }
 
