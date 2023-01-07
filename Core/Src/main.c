f56e63e32771b9cd0cdaa5773fd6ee2d2e7e6c98
TalhaZia1 <TalhaZia1@users.noreply.github.com>
2023-01-07 22:08:25 +0100

    main.c cleanup

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,15 +1,7 @@
 #include "stm32f4xx_hal.h"
-#include "gpio_cfg.h"
-#include "low_power_cfg.h"
-#include "uart_cfg.h"
-#include "bkup_SRAM_cfg.h"
 
 int main(void) {
 	HAL_Init();
-	GPIO_BTN_Polling_Init();
-	UART2_Init();
-	printWelcomeMessage();
-	bkup_SRAM_Init();
 	while (1);
 	return 0; 
 }
